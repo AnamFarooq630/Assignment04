dca082062133a270ba46541b2faf20b7edded361
AnamFarooq630 <247304@students.au.edu.pk>
2026-06-04 00:58:26 +0500

    Created a Authentication State Application

--- App05-AuthState/obj/Debug/net10.0/App05-AuthState.AssemblyInfo.cs
+++ App05-AuthState/obj/Debug/net10.0/App05-AuthState.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App05-AuthState")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c5e94a1135d49266d40a95101dd19bda7be0c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6c217fb50dc89bf3e89b453d394d5f78848997")]
 [assembly: System.Reflection.AssemblyProductAttribute("App05-AuthState")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App05-AuthState")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
